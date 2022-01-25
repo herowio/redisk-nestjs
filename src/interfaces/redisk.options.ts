4c893f0b3f5070fcb721a8dee674a30d5f9ad461
Dlr <david.larouziere@gmail.com>
2022-01-25 11:42:23 +0100

    Using herow-redisk

--- src/interfaces/redisk.options.ts
+++ src/interfaces/redisk.options.ts
@@ -1,7 +1,10 @@
+import {SentinelOptions} from "herow-redisk/dist/client/sentinel.options";
+
 export interface RediskOptions {
     url?: string;
     host?: string;
     port?: number;
     db?: number;
     password?: string;
+    sentinels? : SentinelOptions[];
 }
